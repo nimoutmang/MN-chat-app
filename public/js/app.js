eef4ef50a5e648269cb88891a0564e71dd6814a3
Student <student@PNCLB22-08.pnc.lan>
2021-05-25 08:48:49 +0700

    pull request

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,17 @@
-function displayMessage (event){
-    const message = document.querySelector('#message').value;
-    let cardbody = document.querySelector(".card");
-    let card_bg = document.querySelector(".card-body");
-    cardbody.textContent = message;fv 
-    card_bg.appendChild(cardbody);
+// function displayMessage (event){
+//     const message = document.querySelector('#message').value;
+//     let cardbody = document.querySelector(".card");
+//     let card_bg = document.querySelector(".card-body");
+//     cardbody.textContent = message;fv 
+//     card_bg.appendChild(cardbody);
 
 
     
     
 
    
-}
+// }
 
 
-const btnSend = document.querySelector('.btn-click');
-btnSend.addEventListener('click',displayMessage);
+// const btnSend = document.querySelector('.btn-click');
+// btnSend.addEventListener('click',displayMessage);
